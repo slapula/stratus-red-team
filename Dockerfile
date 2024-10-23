a7dbc2e67e5b4f896532ec6147cda4b26f66462c
Aaron J. Smith <asmith@clarityinnovates.com>
2024-10-22 19:50:50 -0500

    removing problem ec2 check

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ ADD . /build
 RUN make BUILD_VERSION=${VERSION}
 
 FROM alpine:3.20.3@sha256:beefdbd8a1da6d2915566fde36db9db0b524eb737fc57cd1367effd16dc0d06d AS runner
-LABEL org.opencontainers.image.source="https://github.com/DataDog/stratus-red-team/"
+LABEL org.opencontainers.image.source="https://github.com/slapula/stratus-red-team/"
 COPY --from=builder /build/bin/stratus /stratus
 RUN apk add --update git # git is needed for Terraform to download external modules at runtime
 ENTRYPOINT ["/stratus"]
--- v2/internal/providers/aws.go
+++ v2/internal/providers/aws.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/datadog/stratus-red-team/v2/internal/utils"
 	"github.com/google/uuid"
 	"log"
@@ -28,15 +27,6 @@ func (m *AWSProvider) GetConnection() aws.Config {
 }
 
 func (m *AWSProvider) IsAuthenticatedAgainstAWS() bool {
-	// We make a sample API call to AWS to ensure the user is authenticated
-	// Note: We use ec2:DescribeAccountAttributes as an arbitrary API call
-	// instead of sts:GetCallerIdentity, to ensure an AWS region was properly set
-	ec2Client := ec2.NewFromConfig(m.GetConnection())
-	_, err := ec2Client.DescribeAccountAttributes(context.Background(), &ec2.DescribeAccountAttributesInput{})
-	if err != nil {
-		return false
-	}
-
 	// Note: Explicitly setting AWS_REGION/AWS_DEFAULT_REGION is not strictly required for the AWS SDK to work, but it is necessary for Terraform
 	// If it's not set, we get a user-unfriendly error such as the one describe at https://github.com/DataDog/stratus-red-team/issues/506
 	if os.Getenv("AWS_REGION") == "" && os.Getenv("AWS_DEFAULT_REGION") == "" {
